f93a3791ee91022742f84f090a842019862f7c71
Austin Bailey <a604b997@ku.edu>
2017-10-11 12:12:37 -0500

    made vector stuff work

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -6,11 +6,109 @@
 *   @purpose:       Preforms an NFA to DFA conversion
 */
 //./assignment1 < exampleInput.txt
+#include <algorithm>
+#include <functional>
+#include <array>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 #include "State.h"
+
+
+std::vector<int> epsilonClosure(int currentState, std::vector<State*> states, std::map<int,bool> visited)
+{
+    std::vector<int> result;
+
+    return result;
+}
+
+/*
+*   This function is meant to combine sorted vectors while maintaining the
+*   uniqueness of the result vector. That is, all of the elements in the
+*   result vector will be unique
+*/
+std::vector<int> combineIntVectors(std::vector<int> first, std::vector<int> second)
+{
+    std::vector<int> result;
+    while(!second.empty()||!first.empty())
+    {
+        if(first.empty())
+        {
+            result.insert(result.end(),second.begin(),second.end());
+            return result;
+        }
+        if(second.empty())
+        {
+            result.insert(result.end(),first.begin(),first.end());
+            return result;
+        }
+        if(first.front()==second.front())
+        {
+            result.push_back(first.front());
+            first.erase(first.begin());
+            second.erase(second.begin());
+        }
+        else if(first.front()<second.front())
+        {
+            result.push_back(first.front());
+            first.erase(first.begin());
+        }
+        else
+        {
+            result.push_back(second.front());
+            second.erase(second.begin());
+        }
+    }
+    return result;
+}
+
+bool elementIsInside(int element, std::vector<int> container)
+{
+    for(unsigned int i=0; i<container.size(); i++)
+    {
+        if(container.at(i)==element)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+/*
+std::vector<int> epsilonClosureHelper(int currentState, std::vector<State*> States, std::map<int,bool> visited)
+{
+    std::vector<int> result;
+    if(visited[currentState])//if we've already visited this state
+    {
+        return result;
+    }
+    //note: if the map looksup something that isnt there, it returns false
+}
+
+std::vector<int> computeFullEpsilonClosure(int whichState, std::vector<State*> States)
+{
+    std::vector<int> result;
+    if(States.at(whichState)->getEpsilonClosure().size()==0)
+    {
+        return result;
+    }
+
+    std::map<int, bool> visited;//records the states that we have visited
+    visited[whichState] = true;
+//    std::vector<int> tempState;
+    State* tempState;
+    for(int i=0; i< States.at(whichState)->getEpsilonClosure().size(); i++)
+    {
+        //temp state is the ith state in the epsilon Closure
+        tempState = States.at(States.at(whichState)->getEpsilonClosure().at(i));
+//        result.insert(result.end(), temp.begin(), temp.end());
+    }
+
+    std::sort(result.begin(), result.end());
+//    result = std::unique(result.begin(), result.end());
+    return result;
+}
+*/
 
 void printIntVector(std::vector<int> intVec)
 {
@@ -288,48 +386,15 @@ int main()
         NFAStates.push_back(temp);
         temp->clear();
     }
-    /*
-    std::vector<int>* tempVec = new std::vector<int>;
-    State* tempState;
-    for(int i=0; i<numOfNFAStates; i++)
+
+
+
+
+    bool lcv = true;
+    while(lcv)
     {
-        std::cin >> read;
-        if(std::stoi(read)!=(i+1))
-        {
-            std::cout << "Invalid input, column is not numbered appropriately\n";
-            std::cout << "Expected:" << (i+1) << "\n";
-        }
-        tempState = new State(i+1);
-        for(int j = 0; j < numOfNFAStates; j++)//populates the State with all of transitions for the given input
-        {
-            std::cin >> read;
-            if(read.length()!=2)//if the length is 2 we can skip, its empty set
-            {
-                head = 0;
-                read.erase(read.begin(), read.begin()+1);//tears off beggining curly
-                read.erase(read.end()-1, read.end());//tears off end curly
-                for(int i=0; i< read.length(); i++)
-                {
-                    if(i+1==read.length())
-                    {
-                        tempString = read.substr(head,(i+1-head));
-                        //std::cout << "read.substr(head,i): " <<tempString << "\n";
-                        //finalStates.push_back(std::stoi(tempString));
-                        tempVec->push_back(std::stoi(tempString));
-                    }
-                    if(read.at(i)==',')
-                    {
-                        //std::cout << "head indexed: " << head << " Head Value: " << read.at(head) << "\n";
-                        //std::cout << "i index:      " << i    << "  i value:   " << read.at(i) << "\n";
-                        tempString = read.substr(head,i-head);
-                        tempVec->push_back(std::stoi(tempString));
-                        //std::cout << "read.substr(head,i): " <<tempString <<"\n";
-                        head = i+1;
-                    }
-                }
-                tempState->addStates((j+1), *(tempVec));
-            }
-        }
-    }*/
+
+    }
+
     return 0;
 }
